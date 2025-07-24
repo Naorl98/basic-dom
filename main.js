0e97bafb2c5e4789ff2aa52ae7b870f4c9632a41
Naorl98 <Naorman12@gmail.com>
2025-07-24 11:33:35 +0300

    נשךך

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// console.log(document)
+const ball = document.getElementById("ball")
+
+const moveRight = function () {
+  const current = parseInt(ball.style.left) || 0
+  ball.style.left = (current + 15) + "px"
+}
+
+const moveLeft = function () {
+  const current = parseInt(ball.style.left) || 0
+  ball.style.left = (current - 15) + "px"
+}
+
+const moveUp = function () {
+  const current = parseInt(ball.style.top) || 0
+  ball.style.top = (current - 15) + "px"
+}
+
+const moveDown = function () {
+  const current = parseInt(ball.style.top) || 0
+  ball.style.top = (current + 15) + "px"
+}
+
+document.getElementById("right").onclick = moveRight
+document.getElementById("left").onclick = moveLeft
+document.getElementById("up").onclick = moveUp
+document.getElementById("down").onclick = moveDown
+
+
